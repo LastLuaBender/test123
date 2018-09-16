f3ce06d7c28559cd495708c9ce6759901d917cb4
LastLuaBender <43222980+LastLuaBender@users.noreply.github.com>
2018-09-16 16:11:51 +0200

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,16 +1,19 @@
 local AutoUpdater = require 'test'
 
-AutoUpdater.update(
-  {
-    name = 'test2',
-    version = 1,
-    sourceUrl = 'https://raw.githubusercontent.com/LastLuaBender/test123/master/test2.lua',
-    versionUrl = 'https://raw.githubusercontent.com/LastLuaBender/test123/master/test2.version',
-    onUpdated = function()
-      print('test2 updated')
-    end
-  }
-)
-
 local menu = MenuElement({type = MENU, id = 'test2', name = 'test2'})
+
+function OnLoad()
+  AutoUpdater.update(
+    {
+      name = 'test2',
+      version = 1,
+      sourceUrl = 'https://raw.githubusercontent.com/LastLuaBender/test123/master/test2.lua',
+      versionUrl = 'https://raw.githubusercontent.com/LastLuaBender/test123/master/test2.version',
+      onUpdated = function()
+        print('test2 updated')
+      end
+    }
+  )
+end
+
 print('test2 new loaded')
